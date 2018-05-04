5e41f48690d7777c2425bed2b16a2eb685aefcac
Vadim Osipov <osipov.vad@gmail.com>
2018-05-04 14:49:25 +0300

    Add static code analysis plugin

--- src/main/java/com/vosipov/calculator/Calculator.java
+++ src/main/java/com/vosipov/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package com.vosipov.calculator;
 import org.springframework.stereotype.Service;
 
+/**
+* Calculator Service
+*/
 @Service
 public class Calculator {
     int sum(int a, int b) {
--- src/main/java/com/vosipov/calculator/CalculatorApplication.java
+++ src/main/java/com/vosipov/calculator/CalculatorApplication.java
@@ -3,6 +3,9 @@ package com.vosipov.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* Main Spring Applicaiton
+*/
 @SpringBootApplication
 public class CalculatorApplication {
 
